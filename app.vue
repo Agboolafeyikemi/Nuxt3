08fe75a98f6cf5f8a43a13d9594569a566f27c43
Agboolafeyikemi <agboolafeyikemi93@gmail.com>
2022-11-06 20:50:52 +0000

    Genesis Commit

---
+++ app.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="max-w-2xl mx-auto mt-9">
+    <Head>
+      <Title>{{ title }}</Title>
+      <!-- <Meta name="description" :content="title" /> -->
+    </Head>
+    <div
+      class="p-4 max-w-md bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+      <div class="flex justify-between items-center mb-4">
+        <h3
+          class="text-xl font-bold leading-none text-gray-900 dark:text-white">
+          {{ title }}
+        </h3>
+        <a
+          href="#"
+          class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
+          View all
+        </a>
+      </div>
+
+      <div class="flow-root">
+        <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+          <List v-for="winner in winnersList" :key="winner" :winner="winner" />
+        </ul>
+      </div>
+      <InfiniteLoading @infinite="load" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import InfiniteLoading from "v3-infinite-loading";
+import "v3-infinite-loading/lib/style.css";
+
+let winnersList = ref([]);
+let page = ref(1);
+let title = ref("Metawin  Winners List");
+let limit = ref(10);
+
+const load = async ($state) => {
+  try {
+    const response = await fetch(
+      `https://api.dev.platform.metawin.com/sweepstake/winner?skip=${
+        limit.value * page.value
+      }&&take=${limit.value}`
+    );
+    const json = await response.json();
+    if (json.length < 10) $state.complete();
+    else {
+      winnersList.value.push(...json);
+      $state.loaded();
+    }
+    page.value++;
+  } catch (error) {
+    $state.error();
+  }
+};
+</script>
---
+++ components/List.vue
@@ -0,0 +1,38 @@
+<template>
+  <li class="py-3 sm:py-4">
+    <div class="flex items-center space-x-5">
+      <div class="flex-shrink-0">
+        <img
+          class="w-8 h-8 rounded-full"
+          :src="winner.avatarUrl"
+          :alt="winner.displayName" />
+      </div>
+      <div class="flex-1 min-w-0">
+        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+          {{ winner.displayName }}
+        </p>
+        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+          {{ formatDate(winner.date) }}
+        </p>
+      </div>
+      <div class="flex-1 min-w-0 ml-32">
+        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+          {{ winner.prize.value }}
+        </p>
+        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+          {{ winner.prize.name }}
+        </p>
+      </div>
+    </div>
+  </li>
+</template>
+<script setup>
+defineProps({
+  winner: Object,
+});
+
+const formatDate = (localDay) => {
+  const date = new Date(localDay);
+  return date.toString().substring(0, 10);
+};
+</script>
